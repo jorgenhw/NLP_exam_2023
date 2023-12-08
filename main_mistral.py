ee73c294dc89e16245b677c4b7e695c6e8c3afc9
jorgenhw <jrgenhwn1@gmail.com>
2023-12-08 15:36:16 +0100

    1

--- main_mistral.py
+++ main_mistral.py
@@ -4,17 +4,64 @@
 #3# huggingface-cli download TheBloke/OpenHermes-2.5-Mistral-7B-GGUF openhermes-2.5-mistral-7b.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False
 import pandas as pd
 # playing with mistral
-from src.func import load_mistral, generate_dataframe
+from src.func_mistral import load_llm, generate_dataframe, semantic_similarity, generate_paraphrases
 from pathlib import Path
-
-# load csv
-train_data = pd.read_csv(Path.cwd() / 'data' / 'train_data.csv')
+from src.classes import bcolors
 
 
-# Load example data and data to paraphrase (in this case the same data, shouldn't be...)
-examples = pd.read_csv(Path.cwd() / 'data' / 'examples.csv')
+def main():
+    print(f'{bcolors.HEADER}Running main_mistral.py{bcolors.ENDC}')
+    
+    print("--------------------------------------------------")
+    print("--------------------------------------------------")
 
-# loads model -> takes data to paraphrase as input and example paraphrasings as input -> returns dataframe with original and paraphrased text
-df = generate_dataframe(train_data.iloc[:,1], 
-                        load_mistral(Path.cwd() / 'model' / 'openhermes-2.5-mistral-7b.Q4_K_M.gguf'),
-                        'paraphrasings.csv')
+    print("Before continuing, make sure you have run the following in the terminal (if not, end this and do so, making sure you're in the right working directory (see README)): ")
+    print("")
+    print("CT_METAL=1 pip install ctransformers --no-binary ctransformers")
+    print("")
+    print("huggingface-cli download TheBloke/OpenHermes-2.5-Mistral-7B-GGUF openhermes-2.5-mistral-7b.Q4_K_M.gguf --local-dir . --local-dir-use-symlinks False")
+    print("")
+    input("If you have done the above, press Enter to continue...")
+    print("--------------------------------------------------")
+    
+    # load CSV
+    print("Loading data...")
+    train_data = pd.read_csv(Path.cwd() / 'data' / 'train_data.csv')
+
+    # load model
+    model = load_llm(Path.cwd() / 'model' / 'openhermes-2.5-mistral-7b.Q4_K_M.gguf')
+
+    # generate paraphrases
+    paraphrasings = generate_paraphrases(train_data, 'text', model)
+
+    # generate dataframe with original and paraphrased text and save it as a csv in the data folder
+    df = generate_dataframe(train_data, 'text', paraphrasings)
+
+    # generate semantic similarity scores and append them as a column to the dataframe
+    semantic_similarity(df)
+
+    
+if __name__ == "__main__":
+    main()
+
+
+
+# Ask the user for their choice of options
+def process_user_choice():
+    while True:
+        print("#############################################")
+        print(f"{bcolors.OKCYAN}Choose an option (1 / 2):{bcolors.ENDC}")
+        print("#############################################")
+        print("1. Enter your own URL to an article from The Guardian website")
+        print("2. Get the latest The Guardian Briefing")
+        
+        choice = input("Enter the number of your choice: ")
+
+        if choice == '1':
+            url = ask_for_url()
+            return url
+        elif choice == '2':
+            url = get_most_recent_briefing()
+            return url
+        else:
+            print(f"{bcolors.WARNING}Invalid choice. Please choose option 1 or 2.{bcolors.ENDC}")
