6ecfbf2acf6f3e619dd6249fc62d422fa738e77f
nakro <nielskrogsgaard96@gmail.com>
2023-12-08 17:07:42 +0100

    validation of sentiment in paraphrasings

--- niels_nbs/paraphrase_cleaning.py
+++ niels_nbs/paraphrase_cleaning.py
@@ -12,7 +12,7 @@ df_para.rename(columns={'Original': 'text'}, inplace=True)
 # combine data
 df_para['label'] = df_train['label']
 
-df = paraphrase_clean_func(df_para, org_col_name='text', para_col_name='text_paraphrase', new_col_name = 'text_paraphrase_clean', min_length = 0, max_length = 500, min_semantic_similarity = 0.5, max_semantic_similarity = 0.95)
+df = paraphrase_clean_func(df_para, org_col_name='text', para_col_name='text_paraphrase', new_col_name = 'text_paraphrase_clean', min_length = 0, max_length = 500, min_semantic_similarity = 0.7, max_semantic_similarity = 0.95)
 
 # write to csv
 df.to_csv('data/twitter_data_paraphrasings_cleaned.csv', index=False)
--- niels_nbs/paraphrase_sentiment_validation.py
+++ niels_nbs/paraphrase_sentiment_validation.py
@@ -1,12 +1,16 @@
 from danlp.models import load_bert_tone_model
+import pandas as pd
+
 classifier = load_bert_tone_model()
 
-# using the classifier
-classifier.predict('Analysen viser, at økonomien bliver forfærdelig dårlig')
-'''{'analytic': 'objektive', 'polarity': 'negative'}''' 
-classifier.predict('Jeg tror alligvel, det bliver godt')
-'''{'analytic': 'subjektive', 'polarity': 'positive'}'''
+df = pd.read_csv('data/twitter_data_paraphrasings_cleaned.csv', index_col=False)
 
-# Get probabilities and matching classes names
-classifier.predict_proba('Analysen viser, at økonomien bliver forfærdelig dårlig')
-classifier._classes()
+# choose a subset of the data
+df = df.sample(n=10, random_state=1)
+
+# add columns with probabilities
+df['probabilities_text'] = df['text'].apply(lambda x: classifier.predict_proba(x, analytic=False)[0])
+df[['prob_pos_org', 'prob_neu_org', 'prob_neg_org']] = pd.DataFrame(df.probabilities_text.tolist(), index= df.index)
+df['probabilities_para'] = df['text_paraphrase_clean'].apply(lambda x: classifier.predict_proba(x, analytic=False)[0])
+df[['prob_pos_new', 'prob_neu_new', 'prob_neg_new']] = pd.DataFrame(df.probabilities_para.tolist(), index= df.index)
+#classifier._classes()
